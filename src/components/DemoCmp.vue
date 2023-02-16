7c01a436cf5f20d054feff8343b7681d12f1e9a1
Abe Li <abe.li@maersk.com>
2023-02-16 14:16:41 +0800

    add demo component

---
+++ src/components/DemoCmp.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-circular-progress
+    :value="value"
+    size="50px"
+    :thickness="0.22"
+    color="secondary"
+    class="q-ma-md"
+    track-color="light-blue"
+  />
+</template>
+<script lang="ts">
+import { defineComponent, onMounted, ref, toRefs, watch } from "vue";
+const DemoCmp = defineComponent({
+  props: {},
+  emits: [],
+  setup(props, context) {
+    onMounted(() => {
+      const circles: HTMLCollection = document.getElementsByTagName("circle");
+      console.log(111, circles[0]);
+      for (let i = 0; i < circles.length; i++) {
+        circles[i].setAttribute("stroke-linecap", "round");
+      }
+    });
+    return { value: 61 };
+  },
+});
+export default DemoCmp;
+</script>
+<style scoped></style>
