9821a2bd5b161134a6d8176623b632182d08afa2
Abe Li <abe.li@maersk.com>
2023-04-03 15:27:58 +0800

    add new header component

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { Dialog, Loading, Notify, Quasar } from "quasar";
 import "./styles/quasar.scss";
 import "@maersk-global/fonts/maeu/fonts.css";
 import "@quasar/extras/material-icons/material-icons.css";
+import "@quasar/extras/fontawesome-v6/fontawesome-v6.css";
 import i18n from "./assets/i18n/i18n";
 
 const quasarUserOptions = {
---
+++ src/views/data-management/DataMgmtView.vue
@@ -0,0 +1,34 @@
+<template>
+  <common-header-component
+    :titles="titles"
+    :icons="['home', 'search', 'expand']"
+    @onHome="goHome"
+    @onExpand="onExpandClick"
+    @onSearch="onSearch($event)"
+  />
+</template>
+<script lang="ts">
+import CommonHeaderComponent from "@/components/CommonHeaderComponent.vue";
+import router from "@/router";
+import { defineComponent, ref } from "vue";
+const DataMgmtView = defineComponent({
+  components: {
+    CommonHeaderComponent,
+  },
+  setup() {
+    const titles = ref(["Data Management"]);
+    const goHome = () => {
+      router.push("/home");
+    };
+    const onExpandClick = () => {
+      console.log("expand");
+    };
+    const onSearch = (e: string) => {
+      console.log(e);
+    };
+    return { titles, goHome, onExpandClick, onSearch };
+  },
+});
+export default DataMgmtView;
+</script>
+<style scoped lang="scss"></style>
