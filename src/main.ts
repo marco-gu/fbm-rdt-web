fa459c5b3dd3df6777b70ef2d69455144463f3a5
Abe Li <abe.li@maersk.com>
2024-01-03 14:01:21 +0800

    add scanner shell

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import "./styles/index.scss";
 import "./styles/fontawesome-free-6.5.1-web/scss/fontawesome.scss";
 import "./styles/fontawesome-free-6.5.1-web/scss/solid.scss";
 import "./styles/fontawesome-free-6.5.1-web/scss/regular.scss";
+if (process.env.VUE_APP_IS_ANDROID_SIMULATOR === "true") {
+  require("./styles/android.web.scss");
+}
+
 // import "font-awesome/css/font-awesome.min.css";
 // import "font-awesome/scss/font-awesome.scss";
 
@@ -17,10 +21,10 @@ import "./styles/fontawesome-free-6.5.1-web/scss/regular.scss";
 // import "./styles/quasar.scss";
 // import "@quasar/extras/material-icons/material-icons.css";
 // import "@quasar/extras/fontawesome-v6/fontawesome-v6.css";
-const quasarUserOptions = {
-  config: {},
-  plugins: {},
-};
+// const quasarUserOptions = {
+//   config: {},
+//   plugins: {},
+// };
 
 createApp(App)
   .use(store, key)
